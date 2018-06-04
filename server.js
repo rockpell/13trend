a12ab1d62745d12926b82618e5270fc831d10f3c
DESKTOP-80UJBU7\MIT-5 <aythffk@gmail.com>
2018-06-04 16:00:23 +0900

    search 연결 완료(기간 미완성)

--- server.js
+++ server.js
@@ -1,21 +1,184 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var bodyParser = require('body-parser');
+var requestURL = require('request');
+var cheerio = require('cheerio');
+
+var client_id = 'zP4_7yKoudLDOkOsJsgU';
+var client_secret = 'nd1Jp0tcCN';
+
+var api_url = 'https://openapi.naver.com/v1/datalab/search';
 
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
 })
 
 app.use(express.static(__dirname+'/hompage'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+app.set('views', './hompage');
 
 app.get('/', function(request, response) {
-	// Scraping(ToCsv);
 	Scraping(function(text){
 		ToCsv(text, function(){
-			response.sendFile(__dirname + '/hompage/main.html');
+			// response.sendFile(__dirname + '/hompage/main.html');
+			const data = {
+				logOn : false,
+				name : "first"
+			}
+			response.render('main', data, function(err, html){
+				response.send(html);
+			});
 		});
 	});
 });
+
+app.get('/real', function(request, response){
+	const data = {
+		logOn : false,
+		name : "rockpell"
+	}
+	Scraping(function(text){
+		ToCsv(text, function(){
+			// response.sendFile(__dirname + '/hompage/main.html');
+			const data = {
+				logOn : false,
+				name : "first"
+			}
+			response.render('main', data, function(err, html){
+				response.send(html);
+			});
+		});
+	});
+	
+});
+
+app.get('/search', function(request, response){
+	var testData = [];
+	const data = {
+		logOn : false,
+		name : "rockpell",
+		datas : testData
+	}
+	response.render('search', data, function(err, html){
+		response.send(html);
+	});
+});
+
+app.get('/alarm', function(request, response){
+	const data = {
+		logOn : false,
+		name : "rockpell"
+	}
+	response.render('alarm', data, function(err, html){
+		response.send(html);
+	});
+});
+
+app.post('/real', function(request, response){
+	console.log("real");
+	const data = {
+		logOn : false,
+		name : "rockpell"
+	}
+	response.render('main', data, function(err, html){
+		response.send(html);
+	});
+})
+
+app.post('/realIn', function(request, response){
+	console.log(request.body);
+	const data = {
+		logOn : true,
+		name : "rockpell"
+	}
+	response.render('main', data, function(err, html){
+		response.send(html);
+	});
+})
+
+app.post('/search', function(request, response){
+	console.log(request.body);
+	console.log("search");
+	var testData = [];
+	const data = {
+		logOn : false,
+		name : "rockpell",
+		datas : testData
+	}
+	response.render('search', data, function(err, html){
+		response.send(html);
+	});
+	console.log("render search");
+})
+
+app.post('/searchIn', function(request, response){
+	console.log(request.body);
+	console.log("searchIn");
+	var testData = [];
+	const data = {
+		logOn : true,
+		name : "rockpell",
+		datas : testData
+	}
+	response.render('search', data, function(err, html){
+		response.send(html);
+	});
+})
+
+app.post('/searchRequest', function(request, response){
+	console.log(request.body);
+	console.log("searchRequest");
+	var testData = [];
+	RequestSerach(request.body.searchText, testData, function(){
+		const data = {
+			logOn : false,
+			name : "rockpell",
+			datas : testData
+		}
+		response.render('search', data, function(err, html){
+			response.send(html);
+		});
+	});
+})
+
+app.post('/searchRequestIn', function(request, response){
+	console.log(request.body);
+	console.log("searchRequest");
+	var testData = [];
+	RequestSerach(request.body.searchText);
+	const data = {
+		logOn : true,
+		name : "rockpell",
+		datas : testData
+	}
+	response.render('search', data, function(err, html){
+		response.send(html);
+	});
+})
+
+app.post('/alarm', function(request, response){
+	const data = {
+		logOn : false,
+		name : "this is text"
+	}
+	response.render('Alarm', data, function(err, html){
+		response.send(html);
+	});
+})
+
+app.post('/alarmIn', function(request, response){
+	const data = {
+		logOn : true,
+		name : "this is text"
+	}
+	response.render('Alarm', data, function(err, html){
+		response.send(html);
+	});
+})
 
 function ToCsv(csvText, method){
 	fs.writeFile('./hompage/t1.csv', csvText, function(err){
@@ -26,15 +189,12 @@ function ToCsv(csvText, method){
 }
 
 function Scraping(method){
-	var cheerio = require('cheerio');
-	var request = require('request');
-
 	var url = 'https://www.naver.com/';
 
 	var realTimeKeywords = Array();
 	var resultText = "id,value\n";
 
-	request(url, function(error, response, html){
+	requestURL(url, function(error, response, html){
 		if(error){throw error};
 		var $ = cheerio.load(html);
 		// const class_a = $('#ah_roll_area PM_CL_realtimeKeyword_rolling', '#ah_roll PM_CL_realtimeKeyword_rolling_base', '#area_hotkeyword PM_CL_realtimeKeyword_base', '#area_navigation', '#section_navbar' ,'#PM_ID_ct');
@@ -65,4 +225,44 @@ function Scraping(method){
 		method(resultText);
 	});
 
+}
+
+function RequestSerach(requestText, outputData, callback){
+	var request_body = {
+	    "startDate": "2018-05-01",
+	    "endDate": "2018-06-01",
+	    "timeUnit": "week", // data, week, month
+	    "keywordGroups": [
+	        {
+	            "groupName": requestText,
+	            "keywords": [
+	                requestText
+	            ]
+	        }
+	    ],
+	    "device": "pc",
+	    "ages": [
+	        "3"
+	    ],
+	    "gender": "m"
+	};
+
+	requestURL.post({
+        url: api_url,
+        body: JSON.stringify(request_body),
+        headers: {
+            'X-Naver-Client-Id': client_id,
+            'X-Naver-Client-Secret': client_secret,
+            'Content-Type': 'application/json'
+        }
+    },
+    function (error, response, body) {
+        console.log(response.statusCode);
+        // console.log(body);
+        var periodDatas = JSON.parse(body).results[0].data;
+        for(var i = 0; i < Object.keys(periodDatas).length; i++){
+        	outputData.push({x:periodDatas[i].period, y: periodDatas[i].ratio });
+        }
+        callback();
+    });
 }
